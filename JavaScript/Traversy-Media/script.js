10a52861d83d3c2faf5bec5f7d2b144c2bee23a5
Jayson-PAmano <jaysonpa36@gmail.com>
2024-08-03 04:16:22 -0700

    Traversy Media javascript

--- JavaScript/Traversy-Media/script.js
+++ JavaScript/Traversy-Media/script.js
@@ -2,4 +2,34 @@
 //   "display:block; padding: 20px; width:120px; background-color: seagreen;  color: #333;";
 // console.log("%cHello,World", style);
 
-//timelaps: 43:33
+//These values are stored on the stack(memory)
+//Primitive datatypes
+let name = "John Doe";
+let age = 30;
+
+//Reference values are stored in the heap
+const person = {
+  name: "Brad",
+  age: 40,
+};
+let newName = name;
+newName = "Jonathan";
+
+let newPerson = person;
+newPerson.name = "Bradley";
+
+console.log(name, newName);
+console.log(person, newPerson);
+
+// TimeStamp:  1:12:54
+
+// let score = 11;
+
+// if (score < 10) {
+//   while (score < 10) {
+//     console.log(score);
+//     score++;
+//   }
+// } else {
+//   console.log("Score is greater than 10");
+// }
