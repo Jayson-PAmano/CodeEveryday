524c6d676f713637321bdf2d5b506183b593e7ad
Jayson-PAmano <jaysonpa36@gmail.com>
2024-08-07 07:48:51 -0700

    js Operators

---
+++ JavaScript/Traversy-Media/script3.js
@@ -0,0 +1,50 @@
+//TODO: Operators
+//1. Arithmetic operators
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// Concatenation
+
+x = "Hello" + "World";
+
+//Exponent
+x = 2 ** 3;
+
+//Increment
+x = 1;
+// x = x + 1;
+x++;
+
+//Decrement
+x = 1;
+x--;
+
+//Assignment operators
+
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+
+//Comparison Operators
+
+x = 2 == 2;
+x = 2 === "2";
+x = 2 != 2;
+x = 2 !== "2";
+x = 10 > 5;
+x = 10 < 5;
+
+x = 10 >= 5;
+x = 10 <= 5;
+
+console.log(x);
