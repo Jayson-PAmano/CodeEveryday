6e7dd0e2fbacb5246ec024131415f980bb645e50
Jayson-PAmano <jaysonpa36@gmail.com>
2024-08-16 04:43:11 -0700

    js array methods

--- JavaScript/Traversy-Media/script10.js
+++ JavaScript/Traversy-Media/script10.js
@@ -6,13 +6,18 @@ let x;
 const numbers = [12, 45, 33, 29, 39, 22];
 const mixed = [12, "hello", true, null];
 
-//Array Constractor
+//Array Contractors
 const fruits = new Array("apple", "grape", "orange");
 
 // console.log(numbers);
 x = numbers[0];
 
 x = numbers[0] + numbers[3];
+/*
+adds the array e.g [12] from index 0 + from the index 3 of [29]
+  12 + 29 = 41  
+
+*/
 
 x = `My favorite fruit is ${fruits[2]}`;
 
@@ -22,7 +27,7 @@ x = numbers.length;
 
 fruits[2] = "pear";
 
-fruits[3] = "straberry";
+fruits[3] = "strawberry";
 
 fruits[fruits.length] = "blueberry";
 fruits[fruits.length] = "peach";
---
+++ JavaScript/Traversy-Media/script11.js
@@ -0,0 +1,38 @@
+//Array Methods
+let x;
+const arr = [34, 55, 95, 20, 15];
+
+/*Array Manipulation*/
+
+// put an value into the end of the array
+arr.push(100);
+
+// remove value in the end of the array
+arr.pop();
+
+//add value in the beginning of an array
+arr.unshift(10);
+
+//remove the beginning value of an array
+arr.shift();
+
+//reversed the array
+// arr.reverse();
+
+//Mixing string methods with array methods
+x = arr.includes(200);
+
+x = arr.indexOf(15);
+
+//will not change the initial array
+x = arr.slice(1, 4);
+
+//does change the original array , it will delete then from the initial array
+//x = arr.splice(1, 4);
+//x = arr.splice(3, 1);
+
+x = arr.splice(1, 4).reverse().toString().charAt(0);
+
+// console.log(x, arr);
+console.log(x);
+//Time: 2:55:56
