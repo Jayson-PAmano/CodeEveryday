46b51688832aefccc54726f171078e0c9b45723b
Jayson-PAmano <jaysonpa36@gmail.com>
2025-02-20 23:56:07 -0800

    Update index.js

--- Code_Again/js/index.js
+++ Code_Again/js/index.js
@@ -1,3 +1,36 @@
 ChangeTitle = () => {
   document.querySelector("h1").textContent = "Hello, YouTube!";
 };
+const myCar = {
+  carName: "Toyota",
+  carYear: 2020,
+  color: "slate-gray",
+};
+
+const animeCharacter = {
+  name: "Naruto",
+  age: 17,
+  ninjaLevel: 1,
+  favoriteFood: "Ramen",
+  jutsu: "Rasengan",
+};
+const animeCharacter2 = {
+  name: "Sauske",
+  age: 18,
+  ninjaLevel: 3,
+  favoriteFood: "None",
+  jutsu: "Chidori",
+  Info: function () {
+    return this.name + " " + this.jutsu;
+  },
+};
+
+const callCharacter = {
+  fullName: function () {
+    return this.name + " " + this.favoriteFood;
+  },
+};
+
+console.log(callCharacter.fullName.call(animeCharacter));
+console.log(`My car ${myCar.carName}`);
+console.log(animeCharacter2.Info());
